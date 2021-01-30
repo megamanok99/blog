174ca66e194ec4afa993e8971dbdf2aa28cd4fc1
Александр Горбунов <megamanok99@gmail.com>
2021-01-30 17:57:52 +0300

    1st

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
+import axios from "axios";
+
 
 function App() {
+
+ const [cards,setCards]=React.useState([]);
+
+  const getCards= async ()=>{
+    console.log('start')
+    const {data} = await axios.get('https://5c3755177820ff0014d92711.mockapi.io/posts');
+    setCards(data);
+   
+  }
+  console.log(cards);
+
+  React.useEffect(()=>{
+  getCards();
+ 
+  },[]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +29,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Card cards={cards}/>
         <a
           className="App-link"
           href="https://reactjs.org"
